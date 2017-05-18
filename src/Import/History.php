3b0a0a88e957cdb9b8e120e89299120375e9bc08
Aydin Hassan <aydin@hotmail.co.uk>
2017-05-18 14:45:19 +0200

    History

---
+++ src/Import/History.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Jh\Import\Import;
+
+use Jh\Import\Source\Source;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\DB\Adapter\AdapterInterface;
+
+/**
+ * @author Aydin Hassan <aydin@wearejh.com>
+ */
+class History
+{
+    /**
+     * @var AdapterInterface
+     */
+    private $dbAdapter;
+
+    public function __construct(ResourceConnection $resourceConnection)
+    {
+        $this->dbAdapter = $resourceConnection->getConnection();
+    }
+
+    public function isImported(Source $source) : bool
+    {
+        $select = $this->dbAdapter
+            ->select()
+            ->from('jh_import_history', 'COUNT(id)')
+            ->where('source_id = ?', $source->getSourceId())
+            ->where('finished IS NOT NULL');
+
+        return $this->dbAdapter->fetchOne($select) > 0;
+    }
+}
