ad55faf109458e74aaec7fca472d3c78980f539c
Aydin Hassan <aydin@hotmail.co.uk>
2017-05-24 12:21:36 +0200

    Tests for report_handlers config

--- test/Config/ConverterTest.php
+++ test/Config/ConverterTest.php
@@ -39,7 +39,8 @@ END;
                     'writer' => 'Jh\Import\Writer\Product',
                     'type' => 'files',
                     'id_field' => 'sku',
-                    'indexers' => []
+                    'indexers' => [],
+                    'report_handlers' => []
                 ]
             ],
             (new Converter)->convert($domDocument)
@@ -82,7 +83,89 @@ END;
                     'indexers' => [
                         'My\Indexer',
                         'My\OtherIndexer'
-                    ]
+                    ],
+                    'report_handlers' => []
+                ]
+            ],
+            (new Converter)->convert($domDocument)
+        );
+    }
+
+    public function testConvertWithNoAdditionalReportHandlers()
+    {
+        $xml = <<<'END'
+<?xml version="1.0"?>
+<config xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="imports.xsd">
+    <files name="product">
+        <source>Jh\Import\Source\Csv</source>
+        <incoming_directory>jh_import/incoming</incoming_directory>
+        <match_files>rdrive.csv</match_files>
+        <specification>Jh\Import\Specification\Product</specification>
+        <writer>Jh\Import\Writer\Product</writer>
+        <id_field>sku</id_field>
+    </files>
+</config>
+END;
+
+        $domDocument = new \DOMDocument();
+        $domDocument->loadXML($xml);
+
+        self::assertEquals(
+            [
+                'product' => [
+                    'source' => 'Jh\Import\Source\Csv',
+                    'incoming_directory' => 'jh_import/incoming',
+                    'match_files' => 'rdrive.csv',
+                    'specification' => 'Jh\Import\Specification\Product',
+                    'writer' => 'Jh\Import\Writer\Product',
+                    'type' => 'files',
+                    'id_field' => 'sku',
+                    'indexers' => [],
+                    'report_handlers' => []
+                ]
+            ],
+            (new Converter)->convert($domDocument)
+        );
+    }
+
+    public function testConvertWithOptionalReportHandlers()
+    {
+        $xml = <<<'END'
+<?xml version="1.0"?>
+<config xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="imports.xsd">
+    <files name="product">
+        <source>Jh\Import\Source\Csv</source>
+        <incoming_directory>jh_import/incoming</incoming_directory>
+        <match_files>rdrive.csv</match_files>
+        <specification>Jh\Import\Specification\Product</specification>
+        <writer>Jh\Import\Writer\Product</writer>
+        <id_field>sku</id_field>
+        <report_handlers>
+            <report_handler>My\ReportHandler</report_handler>
+            <report_handler>My\OtherReportHandler</report_handler>
+        </report_handlers>
+    </files>
+</config>
+END;
+
+        $domDocument = new \DOMDocument();
+        $domDocument->loadXML($xml);
+
+        self::assertEquals(
+            [
+                'product' => [
+                    'source' => 'Jh\Import\Source\Csv',
+                    'incoming_directory' => 'jh_import/incoming',
+                    'match_files' => 'rdrive.csv',
+                    'specification' => 'Jh\Import\Specification\Product',
+                    'writer' => 'Jh\Import\Writer\Product',
+                    'type' => 'files',
+                    'id_field' => 'sku',
+                    'indexers' => [],
+                    'report_handlers' => [
+                        'My\ReportHandler',
+                        'My\OtherReportHandler'
+                    ],
                 ]
             ],
             (new Converter)->convert($domDocument)
