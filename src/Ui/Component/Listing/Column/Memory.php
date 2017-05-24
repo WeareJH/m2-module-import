cf64b31dd9cafe5bf04c333f0581839e15b1c597
Aydin Hassan <aydin@hotmail.co.uk>
2017-05-24 12:21:05 +0200

    Extract format_bytes function

--- src/Ui/Component/Listing/Column/Memory.php
+++ src/Ui/Component/Listing/Column/Memory.php
@@ -24,25 +24,12 @@ class Memory extends Column
         $dataSource['data']['items'] = array_map(function (array $item) {
 
             if (isset($item[$this->getData('name')])) {
-                $item[$this->getData('name')] = $this->formatBytes($item[$this->getData('name')]);
+                $item[$this->getData('name')] = format_bytes($item[$this->getData('name')]);
             }
 
             return $item;
         }, $dataSource['data']['items']);
 
         return $dataSource;
-    }
-
-    private function formatBytes(string $bytes) : string
-    {
-        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
-
-        $bytes = max($bytes, 0);
-        $pow   = floor(($bytes ? log($bytes) : 0) / log(1024));
-        $pow   = min($pow, count($units) - 1);
-
-        $bytes /= (1 << (10 * $pow));
-
-        return round($bytes, 2) . ' ' . $units[$pow];
     }
 }
---
+++ src/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!function_exists('format_bytes')) {
+    function format_bytes(string $bytes) : string
+    {
+        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+
+        $bytes = max($bytes, 0);
+        $pow   = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow   = min($pow, count($units) - 1);
+
+        $bytes /= (1 << (10 * $pow));
+
+        return round($bytes, 2) . ' ' . $units[$pow];
+    }
+}
