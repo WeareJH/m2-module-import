9bd45f882aead4bab2d96fce921a9347606fcb90
Aydin Hassan <aydin@hotmail.co.uk>
2017-05-24 12:53:56 +0200

    tests for adding custom report handlers

--- test/Report/ReportFactoryTest.php
+++ test/Report/ReportFactoryTest.php
@@ -7,6 +7,7 @@ use Jh\Import\LogLevel;
 use Jh\Import\Progress\CliProgress;
 use Jh\Import\Report\Handler\ConsoleHandler;
 use Jh\Import\Report\Handler\DatabaseHandler;
+use Jh\Import\Report\Handler\Handler;
 use Jh\Import\Report\Report;
 use Jh\Import\Report\ReportFactory;
 use Jh\Import\Source\Iterator;
@@ -135,5 +136,72 @@ class ReportFactoryTest extends TestCase
         self::assertInstanceOf(ConsoleHandler::class, $handlers[1]);
 
         $mock->disable();
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     * @expectedExceptionMessage Report handler must implement "Jh\Import\Report\Handler\Handler"
+     */
+    public function testExceptionIsThrownIfAdditionalHandlerDoesNotImplementCorrectInterface()
+    {
+        $appState  = $this->prophesize(State::class);
+        $appState->getMode()->willReturn(State::MODE_DEVELOPER);
+
+        $services = [
+            State::class => $appState,
+            DatabaseHandler::class => $this->prophesize(DatabaseHandler::class)->reveal(),
+            'some_handler' => new \stdClass
+        ];
+
+        $objectManager = $this->prophesize(ObjectManagerInterface::class);
+        $objectManager->get(Argument::type('string'))->will(function ($args) use ($services) {
+            return $services[$args[0]];
+        });
+
+        $objectManager
+            ->create(ConsoleHandler::class, ['minErrorLevel' => LogLevel::WARNING])
+            ->willReturn(new ConsoleHandler(new CliProgress(new NullOutput), LogLevel::WARNING));
+
+        $factory = new ReportFactory($objectManager->reveal());
+
+        $config = new Config('product', ['report_handlers' => ['some_handler']]);
+        $factory->createFromSourceAndConfig(new Iterator(new \ArrayIterator([])), $config);
+    }
+
+    public function testAdditionalReportHandlersAreAddedToReport()
+    {
+        $appState  = $this->prophesize(State::class);
+        $appState->getMode()->willReturn(State::MODE_DEVELOPER);
+
+
+        $handler1 = $this->prophesize(Handler::class)->reveal();
+        $handler2 = $this->prophesize(Handler::class)->reveal();
+        $services = [
+            State::class => $appState,
+            DatabaseHandler::class => $this->prophesize(DatabaseHandler::class)->reveal(),
+            'some_handler' => $handler1,
+            'some_other_handler' => $handler2
+        ];
+
+        $objectManager = $this->prophesize(ObjectManagerInterface::class);
+        $objectManager->get(Argument::type('string'))->will(function ($args) use ($services) {
+            return $services[$args[0]];
+        });
+
+        $objectManager
+            ->create(ConsoleHandler::class, ['minErrorLevel' => LogLevel::WARNING])
+            ->willReturn(new ConsoleHandler(new CliProgress(new NullOutput), LogLevel::WARNING));
+
+        $factory = new ReportFactory($objectManager->reveal());
+
+        $config = new Config('product', ['report_handlers' => ['some_handler', 'some_other_handler']]);
+        $report = $factory->createFromSourceAndConfig(new Iterator(new \ArrayIterator([])), $config);
+        $handlers = self::readAttribute($report, 'handlers');
+
+        self::assertInstanceOf(Report::class, $report);
+        self::assertCount(4, $handlers);
+        self::assertInstanceOf(ConsoleHandler::class, $handlers[1]);
+        self::assertContains($handler1, $handlers);
+        self::assertContains($handler2, $handlers);
     }
 }
