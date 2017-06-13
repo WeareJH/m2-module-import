ed7740ed155ff97e9e971efa98597bd3d42061ae
Aydin Hassan <aydin@hotmail.co.uk>
2017-06-13 10:18:18 +0200

    CS

--- test/Controller/Adminthml/Config/IndexTest.php
+++ test/Controller/Adminthml/Config/IndexTest.php
@@ -3,18 +3,9 @@
 namespace Jh\ImportTest\Controller\Adminhtml\Config;
 
 use Jh\Import\Controller\Adminhtml\Config\Index;
-use Jh\Import\Entity\ImportHistoryFactory;
 use Jh\UnitTestHelpers\ObjectHelper;
 use Magento\Backend\App\Action\Context;
 use Magento\Framework\View\Result\PageFactory;
-use Magento\Framework\Message\Manager as MessageManager;
-use Magento\Framework\App\Request\Http;
-use Magento\Framework\App\ResponseInterface;
-use Magento\Framework\Controller\ResultFactory;
-use Magento\Framework\ObjectManager\ObjectManager;
-use Magento\Framework\Event\Manager as EventManager;
-use Magento\Backend\Model\View\Result\RedirectFactory;
-use Magento\Backend\Model\View\Result\Redirect;
 use Prophecy\Prophecy\ObjectProphecy;
 use PHPUnit\Framework\TestCase;
 
