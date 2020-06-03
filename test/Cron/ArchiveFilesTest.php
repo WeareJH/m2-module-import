946a8ac4fa9cc647fb4b96dd2618695550ba273b
Aydin Hassan <aydin@hotmail.co.uk>
2020-06-03 15:58:06 +0200

    Fix archive tests

--- test/Cron/ArchiveFilesTest.php
+++ test/Cron/ArchiveFilesTest.php
@@ -80,43 +80,49 @@ class ArchiveFilesTest extends TestCase
         self::assertCount(0, array_diff(scandir($this->tempDirectory . '/archived', SCANDIR_SORT_NONE), ['..', '.']));
     }
 
-//    public function testNoZipCreatedIfFilesNotOlderThan3Days()
-//    {
-//        touch($this->tempDirectory . '/failed/file1.txt');
-//        touch($this->tempDirectory . '/archived/file1.txt');
-//
-//        $builder = new MockBuilder();
-//        $builder->setNamespace('Jh\Import\Cron')
-//            ->setName('time')
-//            ->setFunction(function () {
-//                return 1497434400; //14-6-2017 10:00:00
-//            });
-//
-//        $timeMock = $builder->build();
-//        $timeMock->enable();
-//
-//        $builder = new MockBuilder();
-//        $builder->setNamespace('Jh\Import\Cron')
-//            ->setName('filectime')
-//            ->setFunction(function () {
-//                return 1497348000; //13-6-2017 10:00:00
-//            });
-//
-//        $fileCTimeMock = $builder->build();
-//        $fileCTimeMock->enable();
-//
-//        $this->getCron()->execute();
-//
-//        $fileCTimeMock->disable();
-//        $timeMock->disable();
-//
-//        self::assertFileExists($this->tempDirectory . '/failed/file1.txt');
-//        self::assertFileExists($this->tempDirectory . '/archived/file1.txt');
-//
-//        self::assertCount(1, array_diff(scandir($this->tempDirectory . '/failed', SCANDIR_SORT_NONE), ['..', '.']));
-//        self::assertCount(1, array_diff(scandir($this->tempDirectory . '/archived', SCANDIR_SORT_NONE), ['..', '.']));
-//    }
+    /**
+     * @runInSeparateProcess
+     */
+    public function testNoZipCreatedIfFilesNotOlderThan3Days()
+    {
+        touch($this->tempDirectory . '/failed/file1.txt');
+        touch($this->tempDirectory . '/archived/file1.txt');
 
+        $builder = new MockBuilder();
+        $builder->setNamespace('Jh\Import\Cron')
+            ->setName('time')
+            ->setFunction(function () {
+                return 1497434400; //14-6-2017 10:00:00
+            });
+
+        $timeMock = $builder->build();
+        $timeMock->enable();
+
+        $builder = new MockBuilder();
+        $builder->setNamespace('Jh\Import\Cron')
+            ->setName('filectime')
+            ->setFunction(function () {
+                return 1497348000; //13-6-2017 10:00:00
+            });
+
+        $fileCTimeMock = $builder->build();
+        $fileCTimeMock->enable();
+
+        $this->getCron()->execute();
+
+        $fileCTimeMock->disable();
+        $timeMock->disable();
+
+        self::assertFileExists($this->tempDirectory . '/failed/file1.txt');
+        self::assertFileExists($this->tempDirectory . '/archived/file1.txt');
+
+        self::assertCount(1, array_diff(scandir($this->tempDirectory . '/failed', SCANDIR_SORT_NONE), ['..', '.']));
+        self::assertCount(1, array_diff(scandir($this->tempDirectory . '/archived', SCANDIR_SORT_NONE), ['..', '.']));
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
     public function testOldFilesZipped()
     {
         touch($this->tempDirectory . '/failed/file1.txt');
@@ -176,57 +182,63 @@ class ArchiveFilesTest extends TestCase
         self::assertCount(1, array_diff(scandir($this->tempDirectory . '/archived', SCANDIR_SORT_NONE), ['..', '.']));
     }
 
-//    public function testOnlyFilesOlderThan3DaysIncludedInZip()
-//    {
-//        touch($this->tempDirectory . '/failed/file1.txt');
-//        touch($this->tempDirectory . '/failed/file2.txt');
-//
-//        $builder = new MockBuilder();
-//        $builder->setNamespace('Jh\Import\Cron')
-//            ->setName('time')
-//            ->setFunction(function () {
-//                return 1497434400; //14-6-2017 10:00:00
-//            });
-//
-//        $timeMock = $builder->build();
-//        $timeMock->enable();
-//
-//        $builder = new MockBuilder();
-//        $builder->setNamespace('Jh\Import\Cron')
-//            ->setName('filectime')
-//            ->setFunction(function ($fileName) {
-//                switch ($fileName) {
-//                    case $this->tempDirectory . '/failed/file1.txt':
-//                        return 1496743200; //6-6-2017 10:00:00
-//                    case $this->tempDirectory . '/failed/file2.txt':
-//                        return 1497434400; //14-6-2017 10:00:00
-//                }
-//            });
-//
-//        $fileCTimeMock = $builder->build();
-//        $fileCTimeMock->enable();
-//
-//        $this->getCron()->execute();
-//
-//        $fileCTimeMock->disable();
-//        $timeMock->disable();
-//
-//        self::assertFileNotExists($this->tempDirectory . '/failed/file1.txt');
-//        self::assertFileExists($this->tempDirectory . '/failed/file2.txt');
-//        self::assertFileExists($this->tempDirectory . '/failed/failed-14-06-2017-10-00.zip');
-//
-//        $zip = new \ZipArchive;
-//        $zip->open($this->tempDirectory . '/failed/failed-14-06-2017-10-00.zip');
-//
-//        self::assertEquals(1, $zip->numFiles);
-//        self::assertNotFalse($zip->locateName('file1.txt'));
-//
-//        $zip->close();
-//
-//        self::assertCount(2, array_diff(scandir($this->tempDirectory . '/failed', SCANDIR_SORT_NONE), ['..', '.']));
-//        self::assertCount(0, array_diff(scandir($this->tempDirectory . '/archived', SCANDIR_SORT_NONE), ['..', '.']));
-//    }
+    /**
+     * @runInSeparateProcess
+     */
+    public function testOnlyFilesOlderThan3DaysIncludedInZip()
+    {
+        touch($this->tempDirectory . '/failed/file1.txt');
+        touch($this->tempDirectory . '/failed/file2.txt');
 
+        $builder = new MockBuilder();
+        $builder->setNamespace('Jh\Import\Cron')
+            ->setName('time')
+            ->setFunction(function () {
+                return 1497434400; //14-6-2017 10:00:00
+            });
+
+        $timeMock = $builder->build();
+        $timeMock->enable();
+
+        $builder = new MockBuilder();
+        $builder->setNamespace('Jh\Import\Cron')
+            ->setName('filectime')
+            ->setFunction(function ($fileName) {
+                switch ($fileName) {
+                    case $this->tempDirectory . '/failed/file1.txt':
+                        return 1496743200; //6-6-2017 10:00:00
+                    case $this->tempDirectory . '/failed/file2.txt':
+                        return 1497434400; //14-6-2017 10:00:00
+                }
+            });
+
+        $fileCTimeMock = $builder->build();
+        $fileCTimeMock->enable();
+
+        $this->getCron()->execute();
+
+        $fileCTimeMock->disable();
+        $timeMock->disable();
+
+        self::assertFileNotExists($this->tempDirectory . '/failed/file1.txt');
+        self::assertFileExists($this->tempDirectory . '/failed/file2.txt');
+        self::assertFileExists($this->tempDirectory . '/failed/failed-14-06-2017-10-00.zip');
+
+        $zip = new \ZipArchive;
+        $zip->open($this->tempDirectory . '/failed/failed-14-06-2017-10-00.zip');
+
+        self::assertEquals(1, $zip->numFiles);
+        self::assertNotFalse($zip->locateName('file1.txt'));
+
+        $zip->close();
+
+        self::assertCount(2, array_diff(scandir($this->tempDirectory . '/failed', SCANDIR_SORT_NONE), ['..', '.']));
+        self::assertCount(0, array_diff(scandir($this->tempDirectory . '/archived', SCANDIR_SORT_NONE), ['..', '.']));
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
     public function testOnlyImportWithArchiveOldFilesFlagAreArchived()
     {
         $config = [
