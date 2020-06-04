407cbd35f2dd3403c440c69177eed60ac56e57cf
Aydin Hassan <aydin@hotmail.co.uk>
2020-06-04 17:07:20 +0200

    Source factory

---
+++ src/Source/SourceFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jh\Import\Source;
+
+use Jh\Import\Config;
+use Magento\Framework\ObjectManagerInterface;
+
+class SourceFactory
+{
+    /**
+     * @var ObjectManagerInterface
+     */
+    private $objectManager;
+
+    public function __construct(ObjectManagerInterface $objectManager)
+    {
+        $this->objectManager = $objectManager;
+    }
+
+    public function create(Config $config, array $args) : Source
+    {
+        $source = $this->objectManager
+            ->create($config->getSourceService(), $args);
+
+        if (!$source instanceof Source) {
+            throw new \RuntimeException(sprintf('Source must implement "%s"', Source::class));
+        }
+
+        return $source;
+    }
+}
