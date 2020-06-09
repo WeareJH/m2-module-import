07e403a98d6a8adc8baeafb38fbc180b67df23d4
Aydin Hassan <aydin@hotmail.co.uk>
2020-06-09 20:51:41 +0200

    Add only method to record + type/doc cleanup

--- test/Import/RecordTest.php
+++ test/Import/RecordTest.php
@@ -5,18 +5,15 @@ namespace Jh\ImportTest\Import;
 use Jh\Import\Import\Record;
 use PHPUnit\Framework\TestCase;
 
-/**
- * @author Aydin Hassan <aydin@wearejh.com>
- */
 class RecordTest extends TestCase
 {
-    public function testGetRowNumber()
+    public function testGetRowNumber(): void
     {
         $record = new Record(10, []);
         self::assertEquals(10, $record->getRowNumber());
     }
 
-    public function testSetColumnValue()
+    public function testSetColumnValue(): void
     {
         $record = new Record(10, []);
         self::assertEquals(null, $record->getColumnValue('column1'));
@@ -25,7 +22,7 @@ class RecordTest extends TestCase
         self::assertEquals(null, $record->getColumnValue('value'));
     }
 
-    public function testUnset()
+    public function testUnset(): void
     {
         $record = new Record(10, ['column1' => 'value']);
 
@@ -33,7 +30,7 @@ class RecordTest extends TestCase
         self::assertNull($record->getColumnValue('column1'));
     }
 
-    public function testUnsetMany()
+    public function testUnsetMany(): void
     {
         $record = new Record(10, ['column1' => 'value1', 'column2' => 'value2']);
 
@@ -42,7 +39,7 @@ class RecordTest extends TestCase
         self::assertNull($record->getColumnValue('column2'));
     }
 
-    public function testGetColumnValue()
+    public function testGetColumnValue(): void
     {
         $record = new Record(10, ['column1' => 'value1', 'column2' => 'value2']);
 
@@ -50,7 +47,7 @@ class RecordTest extends TestCase
         self::assertEquals('value2', $record->getColumnValue('column2'));
     }
 
-    public function testGetColumnValueReturnsDefaultIfNotSet()
+    public function testGetColumnValueReturnsDefaultIfNotSet(): void
     {
         $record = new Record(10, []);
 
@@ -58,7 +55,7 @@ class RecordTest extends TestCase
         self::assertEquals([], $record->getColumnValue('column1', []));
     }
 
-    public function testGetColumnValueThrowsExceptionIfGivenExpectedTypeDoesNotMatch()
+    public function testGetColumnValueThrowsExceptionIfGivenExpectedTypeDoesNotMatch(): void
     {
         $this->expectException(\RuntimeException::class);
         $this->expectExceptionMessage('Value of "column1" data type: "string" does not match expected: "array"');
@@ -67,13 +64,13 @@ class RecordTest extends TestCase
         $record->getColumnValue('column1', null, 'array');
     }
 
-    public function testGetColumnValuePerformsTypeCheckIfSpecified()
+    public function testGetColumnValuePerformsTypeCheckIfSpecified(): void
     {
         $record = new Record(10, ['column1' => ['key' => 'value']], 'array');
         self::assertEquals(['key' => 'value'], $record->getColumnValue('column1', null, 'array'));
     }
 
-    public function testGetColumnAndUnset()
+    public function testGetColumnAndUnset(): void
     {
         $record = new Record(10, ['column1' => 'value1', 'column2' => 'value2']);
 
@@ -83,7 +80,7 @@ class RecordTest extends TestCase
         self::assertNull($record->getColumnValue('column2'));
     }
 
-    public function testGetColumnAndUnsetReturnsDefaultIfNotSet()
+    public function testGetColumnAndUnsetReturnsDefaultIfNotSet(): void
     {
         $record = new Record(10, []);
 
@@ -91,7 +88,7 @@ class RecordTest extends TestCase
         self::assertEquals([], $record->getColumnValueAndUnset('column1', []));
     }
 
-    public function testGetColumnAndUnsetThrowsExceptionIfGivenExpectedTypeDoesNotMatchAndDoesNotUnsetValue()
+    public function testGetColumnAndUnsetThrowsExceptionIfGivenExpectedTypeDoesNotMatchAndDoesNotUnsetValue(): void
     {
         $record = new Record(10, ['column1' => 'some value']);
         try {
@@ -108,27 +105,27 @@ class RecordTest extends TestCase
         self::fail('RuntimeException not thrown');
     }
 
-    public function testGetColumnAndUnsetPerformsTypeCheckIfSpecified()
+    public function testGetColumnAndUnsetPerformsTypeCheckIfSpecified(): void
     {
         $record = new Record(10, ['column1' => ['key' => 'value']], 'array');
         self::assertEquals(['key' => 'value'], $record->getColumnValueAndUnset('column1', null, 'array'));
         self::assertNull($record->getColumnValue('column1'));
     }
 
-    public function testColumnExists()
+    public function testColumnExists(): void
     {
         $record = new Record(10, ['column1' => 'value']);
         self::assertFalse($record->columnExists('column2'));
         self::assertTrue($record->columnExists('column1'));
     }
 
-    public function testAsArray()
+    public function testAsArray(): void
     {
         $record = new Record(10, ['column1' => 'value1', 'column2' => 'value2']);
         self::assertEquals(['column1' => 'value1', 'column2' => 'value2'], $record->asArray());
     }
 
-    public function testTransform()
+    public function testTransform(): void
     {
         $record = new Record(10, ['column1' => 2]);
         $record->transform('column1', function ($value) {
@@ -137,7 +134,7 @@ class RecordTest extends TestCase
         self::assertEquals(4, $record->getColumnValue('column1'));
     }
 
-    public function testRenameColumn()
+    public function testRenameColumn(): void
     {
         $record = new Record(10, ['column1' => 2]);
         $record->renameColumn('column1', 'column2');
@@ -146,7 +143,7 @@ class RecordTest extends TestCase
         self::assertEquals(2, $record->getColumnValue('column2'));
     }
 
-    public function testMoveColumnToArrayCreatesArrayWhenItDoesNotExistAndUsesOriginalKey()
+    public function testMoveColumnToArrayCreatesArrayWhenItDoesNotExistAndUsesOriginalKey(): void
     {
         $record = new Record(10, ['column1' => 2]);
         $record->moveColumnToArray('column1', 'attributes');
@@ -154,7 +151,7 @@ class RecordTest extends TestCase
         self::assertEquals(['column1' => 2], $record->getColumnValue('attributes'));
     }
 
-    public function testMoveColumnToArrayAddsToArrayIfItExistsAndUsesOriginalKey()
+    public function testMoveColumnToArrayAddsToArrayIfItExistsAndUsesOriginalKey(): void
     {
         $record = new Record(10, ['column1' => 2, 'attributes' => ['brand' => 'apple']]);
         $record->moveColumnToArray('column1', 'attributes');
@@ -162,7 +159,7 @@ class RecordTest extends TestCase
         self::assertEquals(['brand' => 'apple', 'column1' => 2], $record->getColumnValue('attributes'));
     }
 
-    public function testMoveColumnToArrayWithNewKey()
+    public function testMoveColumnToArrayWithNewKey(): void
     {
         $record = new Record(10, ['column1' => 2]);
         $record->moveColumnToArray('column1', 'attributes', 'new-column-name');
@@ -170,7 +167,7 @@ class RecordTest extends TestCase
         self::assertEquals(['new-column-name' => 2], $record->getColumnValue('attributes'));
     }
 
-    public function testMoveMultipleColumnsToArray()
+    public function testMoveMultipleColumnsToArray(): void
     {
         $record = new Record(10, ['column1' => 2, 'column2' => 4]);
         $record->moveMultipleColumnsToArray(['column1', 'column2'], 'attributes');
@@ -179,11 +176,19 @@ class RecordTest extends TestCase
         self::assertEquals(['column1' => 2, 'column2' => 4], $record->getColumnValue('attributes'));
     }
 
-    public function testAddValueToArray()
+    public function testAddValueToArray(): void
     {
         $record = new Record(10, ['attributes' => ['brand' => 'apple']]);
         $record->addValueToArray('attributes', 'bear-species', 'spectacled');
 
         self::assertEquals(['brand' => 'apple', 'bear-species' => 'spectacled'], $record->getColumnValue('attributes'));
+    }
+
+    public function testOnlyRemoveAllUnspecifiedColumns(): void
+    {
+        $record = new Record(10, ['column1' => 'value1', 'column2' => 'value2', 'column3' => 'value3']);
+        $record->only('column1', 'column2');
+
+        self::assertEquals(['column1' => 'value1', 'column2' => 'value2'], $record->asArray());
     }
 }
