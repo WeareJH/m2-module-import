6fc93f43ccf593ca93d84a040f3c81b67eb9e816
Aydin Hassan <aydin@hotmail.co.uk>
2020-06-19 11:43:01 +0200

    Progress factory

---
+++ src/Progress/Factory.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jh\Import\Progress;
+
+use Magento\Framework\App\State;
+
+class Factory
+{
+    /**
+     * @var CliProgress
+     */
+    private $cliProgress;
+    /**
+     * @var State
+     */
+    private $appState;
+
+    public function __construct(State $appState, CliProgress $cliProgress)
+    {
+        $this->appState = $appState;
+        $this->cliProgress = $cliProgress;
+    }
+
+    public function get(): Progress
+    {
+        if ($this->appState->getMode() === State::MODE_DEVELOPER || posix_isatty(STDOUT)) {
+            return $this->getCliProgress();
+        }
+
+        return new NullProgress();
+    }
+
+    public function getCliProgress(): CliProgress
+    {
+        return $this->cliProgress;
+    }
+}
